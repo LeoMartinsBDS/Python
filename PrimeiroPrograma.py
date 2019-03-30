ef14260e435b275390fa692ec3fed881810cce1c
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-30 17:07:08 -0300

    Started studies, print, vars etc

---
+++ PrimeiroPrograma.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#para evitar problemas com acento
+#isto é um comentário
+
+print("Hello world!")
+print("Another message")
+print("Olá mundo!")
+
+print(2+2)
+print(2-2)
+print(2/2)
+print(2*2)
+# exponenciacao
+print(2 ** 3)
+# modulo
+print(10 % 3)
+
+my_var = "Hello word" #string
+print(my_var)
+
+var1 = 1 # integer var
+print(var1)
+
+var2 = 1.1 # float var
+print(var2)
+
+var3 = True # bool var
+var4 = False
+
+print(var3)
+print(var4)
+
+
+"""
+comentario de muitas linhas
+"""
